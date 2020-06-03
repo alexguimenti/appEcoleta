52fd2dc138092c708706deda48a45a11d20f4d5a
Alexandre Guimenti <alexguimenti@gmail.com>
2020-06-03 15:58:55 -0300

    add map pin

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker } from "react-leaflet";
 import axios from "axios";
+import { LeafletMouseEvent } from 'leaflet';
 import api from "../../services/api";
 
 import "./styles.css";
@@ -32,6 +33,7 @@ const CreatePoint = () => {
 
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
 
   useEffect(() => {
     api.get("items").then((response) => {
@@ -74,10 +76,17 @@ const CreatePoint = () => {
     setSelectedUf(uf);
   }
 
-  function handleSelectCuty(event: ChangeEvent<HTMLSelectElement>) {
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
     const city = event.target.value;
 
-    setSelectedUf(city);
+    setSelectedCity(city);
+  }
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    setSelectedPosition([
+      event.latlng.lat,
+      event.latlng.lng,
+    ])
   }
 
   return (
@@ -125,13 +134,13 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-23.5347613, -46.6638152]} zoom={15}>
+          <Map center={[-23.5347613, -46.6638152]} zoom={15} onClick={handleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-            <Marker position={[-23.5347613, -46.6638152]} />
+            <Marker position={selectedPosition} />
           </Map>
 
           <div className="field-group">
@@ -157,7 +166,7 @@ const CreatePoint = () => {
               name="city" 
               id="city"
               value={selectedCity}
-              onChange={handleSelectCuty}>
+              onChange={handleSelectCity}>
           
                 <option value="0">Selecione uma cidade</option>
                 {cities.map((city) => (
