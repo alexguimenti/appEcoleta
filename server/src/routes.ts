4416540e04bce691d9eaf6add5c0304716934601
Alexandre Guimenti <alexguimenti@gmail.com>
2020-06-05 11:05:45 -0300

    add verification on create point route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,10 @@
-import express from 'express'
-import multer from 'multer';
-import multerConfig from './config/multer'
+import express from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
+import { celebrate, Joi } from "celebrate";
 
-import PointsController from './controllers/PointsController';
-import ItemsController from './controllers/ItemsController';
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
 
 // index, show, create, update, delete
 
@@ -13,10 +14,29 @@ const upload = multer(multerConfig);
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get('/items', itemsController.index);
-routes.get('/points', pointsController.index)
-routes.get('/points/:id', pointsController.show)
+routes.get("/items", itemsController.index);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
 
-routes.post('/points', upload.single('image'),pointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        item: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  pointsController.create
+);
 
 export default routes;
